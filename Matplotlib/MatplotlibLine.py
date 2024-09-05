5f4ad80e879654c65c0a766584f0cd9ec7c8eb95
Dineshchand Geetha Ravichandran <>
2024-09-05 21:32:05 +0800

    adding files

--- Matplotlib/MatplotlibLine.py
+++ Matplotlib/MatplotlibLine.py
@@ -3,14 +3,14 @@ import numpy as np
 
 ypoints = np.array([3, 8, 1, 10])
 
-plt.plot(ypoints, linestyle = 'dotted')
+plt.plot(ypoints, linestyle = 'dotted', marker = 'o')
 plt.show()
 
-plt.plot(ypoints, linestyle = 'dashed', linewidth = '20.5')
+plt.plot(ypoints, linestyle = 'dashed', linewidth = '20.5', marker = 'o')
 plt.show()
 
-plt.plot(ypoints, ls = '-.')
+plt.plot(ypoints, ls = '-.', marker = 'o')
 plt.show()
 
-plt.plot(ypoints, color = 'r')
+plt.plot(ypoints, color = 'r', marker = 'o')
 plt.show()
--- Matplotlib/MatplotlibMarkers.py
+++ Matplotlib/MatplotlibMarkers.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 xpoints = np.array([0, 3, 1, 8])
-ypoints = np.array([3, 8, 5, 10])
+ypoints = np.array([3, 5, 7, 10])
 plt.plot(xpoints, ypoints, marker = 'o')
 plt.show()
